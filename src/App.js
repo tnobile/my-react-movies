5050eb21a39b59da32e4b838a825fb475357599b
tnobile <takao.nosaka@outlook.com>
2021-05-03 00:12:51 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import './App.css';
 import Home from './component/Home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home/>
-     </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/people">People</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/people">
+            <Users/>
+          </Route>
+          <Route path="/">
+            <div className="App">
+              <Home /></div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>People</h2>;
 }
 
 export default App;
